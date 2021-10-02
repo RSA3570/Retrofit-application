b4ab2b7a80b44363c7a40dd4a33c24f4fd25c107
krrou <kr.roushan.info@gmail.com>
2021-10-02 18:29:51 +0530

    Retrofit Application with MVVM

---
+++ app/src/main/java/com/example/retrofitapplication/QuoteList.kt
@@ -0,0 +1,10 @@
+package com.example.retrofitapplication
+
+data class QuoteList(
+    val count: Int,
+    val lastItemIndex: Int,
+    val page: Int,
+    val results: List<Result>,
+    val totalCount: Int,
+    val totalPages: Int
+)
